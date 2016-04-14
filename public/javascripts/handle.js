8cb458e27da32f8ce83e734aafdb3e69c8504ca5
kriv <krivitskiy_d@mail.ru>
2016-04-14 17:56:01 +0300

    animation

--- public/javascripts/handle.js
+++ public/javascripts/handle.js
@@ -4,6 +4,11 @@
 $(document).ready(function(){
     $("button, input[type='button']").button();
     $("#menu, #admin_menu > ul").menu();
+    $(document).tooltip({
+        position: {
+            my: "center",
+        }
+    });
 
     $("#logo").on("click", function(){
         $("#menu").slideDown("fast");
@@ -27,14 +32,34 @@ $(document).ready(function(){
     });
 
     $(".animate").on("click",  function(){
+        var state = $(this).attr("state"),
+            num = $(this).attr("num"),
+            wrapper = $(this).parents(".production_wrapper");
+        
+        if(state == '0'){
+            $(this).attr('state', '1').val('stop');
+            wrapper.find(".arrow").addClass("animated");
+            if(num == "1"){
+                wrapper.find(".led").removeClass("red_led").addClass("green_led");
+            }
+        }else{
+            $(this).attr('state', '0').val('start');
+            wrapper.find(".arrow").removeClass("animated");
+            if(num == "1"){
+                wrapper.find(".led").removeClass("green_led").addClass("red_led");
+            }
+        }
+
+    });
+
+    $(".animate_bunker").on("click",  function(){
         var state = $(this).attr("state");
         if(state == '0'){
             $(this).attr('state', '1').val('stop');
-            $(this).parents(".production_wrapper").find(".arrow").addClass("animated");
+            $("#bunker_filler").addClass("fill");
         }else{
             $(this).attr('state', '0').val('start');
-            $(this).parents(".production_wrapper").find(".arrow").removeClass("animated");
+            $("#bunker_filler").removeClass("fill");
         }
-
     });
 });
