961dc94a1b9a57255891f44462089eb305ff1945
kriv <krivitskiy_d@mail.ru>
2016-09-14 13:19:29 +0300

    scale hw_logs

--- bin/ws.js
+++ bin/ws.js
@@ -175,7 +175,7 @@ var server = ws.createServer(function(conn){
             console.log("SCALE MAC: "+scale_mac);
             try{
                 db_conn.query('INSERT INTO scale_log SET zaslon_id = "'+scale_mac+'", state = "'+obj.state+'"');
-                db_conn.query('INSERT INTO hw_log_table SET mac="'+scale_mac+'", message="'+obj.log_string+'", system="'+obj.from+'" ');
+                db_conn.query('INSERT INTO hw_log_table SET mac="'+scale_mac+'", message="change scale state: '+obj.state+'", system="'+obj.from+'" ');
                 www.eventEmitter.emit('scale_led', {state:obj.state, mac:scale_mac});
             }catch(e){
                 console.log("Error: "+e);
