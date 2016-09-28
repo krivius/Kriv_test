bbc6efb6c52686a29712d7902963fa7724e9fa4d
kriv <krivitskiy_d@mail.ru>
2016-09-28 16:45:00 +0300

    scale avg onLoad

--- public/javascripts/socket_handle.js
+++ public/javascripts/socket_handle.js
@@ -462,7 +462,7 @@ socket.on("template_list",  function(data){
 
 
 socket.on("show_scale_avg",  function(data){
-   // console.log(data);
+   console.log(data);
     $.each(data,  function(item){
        $(".informer[mac='"+item.mac+"']").empty().text(item.total);
     });
