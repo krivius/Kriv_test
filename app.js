5df45d813f7a050a12f84d87658aa852d3763d2d
kriv <krivitskiy_d@mail.ru>
2016-10-06 11:30:17 +0300

    profiler

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('look').start(5959, '127.0.0.1');
+require('look').start(5959, '192.168.19.2');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
