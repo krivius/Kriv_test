65bc7d011408fced764e4b9a8dd4081109950671
Treha <3dn@live.ru>
2016-10-07 13:10:22 +0300

    DT

---
+++ bin/DevTools_engine.js
@@ -0,0 +1,12 @@
+var usage   = require('usage');
+var process = require('process');
+
+var pid = process.pid;
+
+function Get_Proc_Stat(){
+    usage.lookup(pid, function (err, result) {
+        console.log(result);
+    });
+}
+
+module.exports = Get_Proc_Stat;
