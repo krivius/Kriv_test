7357c9d838c0aab01141893e7b231b64e621cd63
Treha <3dn@live.ru>
2016-10-07 15:52:53 +0300

    DevTools

--- public/javascripts/socket_handle.js
+++ public/javascripts/socket_handle.js
@@ -321,7 +321,7 @@ socket.on("w_memory",  function(data){
             {
                 type: 'area',
                 name: '3.5 Кг/ч',
-                data: data
+                data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
             },
             {
                 type: 'area',
