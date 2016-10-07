031c135625327e81ba9daeecfd693bc2e5591ee5
Treha <3dn@live.ru>
2016-10-07 17:02:53 +0300

    DevTools

--- public/javascripts/socket_handle.js
+++ public/javascripts/socket_handle.js
@@ -291,10 +291,6 @@ $(function() {
         title: {
             text: 'Memory'
         },
-        subtitle: {
-            text: document.ontouchstart === undefined ?
-                'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
-        },
         xAxis: {
             type: 'datetime'
         },
@@ -306,42 +302,10 @@ $(function() {
         legend: {
             enabled: true
         },
-        plotOptions: {
-            area: {
-                fillColor: {
-                    linearGradient: {
-                        x1: 0,
-                        y1: 0,
-                        x2: 0,
-                        y2: 1
-                    },
-                    stops: [
-                        [0, Highcharts.getOptions().colors[0]],
-                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                    ]
-                },
-                marker: {
-                    radius: 2
-                },
-                lineWidth: 1,
-                states: {
-                    hover: {
-                        lineWidth: 1
-                    }
-                },
-                threshold: null
-            }
-        },
         series: [
             {
                 type: 'area',
-                name: '3.5 Кг/ч',
-                data: [3]
-            },
-            {
-                type: 'area',
-                name: '2.4 Кг/ч',
-                data: []
+                name: 'Mb'
             }
         ]
     });
