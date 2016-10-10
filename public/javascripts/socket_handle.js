87c2a3527665733597fe70863c53664b954f2187
Treha <3dn@live.ru>
2016-10-10 14:57:01 +0300

    DevTools

--- public/javascripts/socket_handle.js
+++ public/javascripts/socket_handle.js
@@ -275,6 +275,7 @@ $(function() {
         });
         var data = [];
         socket.on("system_state_history", function (d) {
+            d.reverse();
             d.forEach(function (item) {
                 data.push({
                     x: item.time,
