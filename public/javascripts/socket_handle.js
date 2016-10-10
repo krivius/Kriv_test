a88a8fceb45055cfb9842cf95ab9d3f97be7d218
Treha <3dn@live.ru>
2016-10-10 12:14:18 +0300

    DevTools

--- public/javascripts/socket_handle.js
+++ public/javascripts/socket_handle.js
@@ -325,7 +325,7 @@ $(function() {
                         for (i = -19; i <= 0; i++) {
                             data.push({
                                 x: time + i * 1000,
-                                y: d.item.memory
+                                y: d[i].memory
                         });
                         }
                     });
