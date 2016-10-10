7e66dcac36836cbbc10af38f60d5deb88c1b08a2
Treha <3dn@live.ru>
2016-10-10 14:02:20 +0300

    DevTools

--- bin/DevTools_engine.js
+++ bin/DevTools_engine.js
@@ -11,7 +11,7 @@ var pid = process.pid;
 
 function Get_Proc_Stat(){
     usage.lookup(pid, function (err, result) {
-        db_conn.query('INSERT INTO dt_sys_monitor(dt_memory, dt_cpu) VALUES(' + (result.memory/1024) + ', ' + result.cpu + ');');
+        db_conn.query('INSERT INTO dt_sys_monitor(dt_memory, dt_cpu, dt_time) VALUES(' + (result.memory/1024) + ', ' + result.cpu + ', unix_timestamp(now()));');
         www.eventEmitter.emit('w_memory', result.memory);
         www.eventEmitter.emit('w_cpu', result.cpu);
     });
